89a52ee6856d629dd68ccc64ce42f2a6d8d82de7
Afua <hi@aoa4eva.me>
2017-12-23 09:15:35 +0000

    Git and Sublime Text scripts

---
+++ Powershell/sublimetext.ps1
@@ -0,0 +1,12 @@
+<# Modified gitdownload for Sublime Text.
+	Research: An arry (or smomething) of files, so that this can be done with a 'for' statement. 
+	That would be interesting. 
+#>
+$sublimedownload="https://download.sublimetext.com/Sublime%20Text%20Build%203143%20x64%20Setup.exe"
+set-executionpolicy remotesigned
+$saveas = "c:\users\aoa4eva\downloads\sublimetext.exe"
+$startop = Get-Date
+Invoke-WebRequest -Uri $sublimedownload -OutFile $saveas
+Write-Output "This operation took $((Get-Date).Subtract($startop).Seconds) seconds to execute"
+& $saveas | Out-Null
+Remove-Item -path $saveas -recurse -force
